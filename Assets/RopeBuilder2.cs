33f91af3fe0f09c9aafc362aabc36a27db124365
Armin Rigo <arigo@tunes.org>
2017-04-05 11:09:32 +0200

    More attempts at making a rope

---
+++ Assets/RopeBuilder2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeBuilder2 : MonoBehaviour {
+
+    public Rigidbody firstLink;
+    public GameObject handle;
+    public int nbSegments = 30;
+    public float jointForce = 10;
+
+    Rigidbody[] links;
+    Vector3 org_position;
+    float global_scale;
+
+	private void Start()
+    {
+        links = new Rigidbody[nbSegments];
+        links[0] = firstLink;
+        org_position = handle.transform.InverseTransformPoint(firstLink.transform.position);
+        firstLink.isKinematic = true;
+
+        Vector3 pos = firstLink.transform.localPosition;
+
+        for (int i = 1; i < nbSegments; i++)
+        {
+            Rigidbody rb = Instantiate<Rigidbody>(firstLink, transform);
+            pos.z += 1;
+            rb.transform.localPosition = pos;
+            rb.isKinematic = false;
+            links[i] = rb;
+        }
+
+        global_scale = transform.lossyScale.magnitude * 0.501f;
+    }
+
+    private void Update()
+    {
+        Vector3 pos = firstLink.position = handle.transform.TransformPoint(org_position);
+        Vector3 forward = handle.transform.forward;
+
+        for (int i = 1; i < nbSegments; i++)
+        {
+            Rigidbody rb = links[i];
+            Vector3 forward2 = rb.position - pos;
+            float angle = Vector3.Angle(forward, forward2);
+            forward = Vector3.Slerp(forward, forward2, 10 / angle);
+            forward.Normalize();
+            pos += forward * global_scale;
+            rb.transform.position = pos;
+        }
+    }
+}
---
+++ Assets/RopeBuilder3.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeBuilder3 : MonoBehaviour {
+
+    public Rigidbody firstLink;
+    public GameObject handle;
+    public int nbSegments = 30;
+    public bool closedLoop = false;
+
+    Rigidbody[] links;
+    Vector3 org_position;
+
+    private void Start()
+    {
+        links = new Rigidbody[nbSegments];
+        links[0] = firstLink;
+        firstLink.isKinematic = true;
+
+        Vector3 pos = firstLink.transform.localPosition;
+
+        for (int i = 1; i < nbSegments; i++)
+        {
+            Rigidbody rb = Instantiate<Rigidbody>(firstLink, transform);
+            pos.z += 2f;
+            rb.transform.localPosition = pos;
+            rb.isKinematic = false;
+            rb.GetComponent<Joint>().connectedBody = links[i - 1];
+            links[i] = rb;
+        }
+
+        if (closedLoop)
+        {
+            links[0].GetComponent<Joint>().connectedBody = links[nbSegments - 1];
+        }
+
+        org_position = handle.transform.InverseTransformPoint(firstLink.transform.position);
+    }
+
+    private void Update()
+    {
+        firstLink.position = handle.transform.TransformPoint(org_position);
+    }
+}
