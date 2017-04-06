dba7149a35576b00f1ab8548641724081f1c905d
Armin Rigo <arigo@tunes.org>
2017-04-06 12:04:39 +0200

    "Rope" start to work well... well, more like a long spring.

--- Assets/RopeBuilder3.cs
+++ Assets/RopeBuilder3.cs
@@ -26,6 +26,7 @@ public class RopeBuilder3 : MonoBehaviour {
             pos.z += 2f;
             rb.transform.localPosition = pos;
             rb.isKinematic = false;
+            //rb.GetComponent<Joint>().connectedAnchor = Vector3.zero;
             rb.GetComponent<Joint>().connectedBody = links[i - 1];
             links[i] = rb;
         }
@@ -41,5 +42,40 @@ public class RopeBuilder3 : MonoBehaviour {
     private void Update()
     {
         firstLink.position = handle.transform.TransformPoint(org_position);
+
+        Vector3 p0 = links[0].position;
+        Vector3 p1 = Vector3.Lerp(links[0].position, links[1].position, 1f / 3f);
+        int nbSteps = 4;
+        float one_step = 1f / nbSteps;
+        Vector3[] vertices = new Vector3[(nbSegments-1) * nbSteps + 1];
+        int nbVertices = 0;
+        vertices[nbVertices++] = p0;
+
+        for (int i = 1; i < nbSegments; i++)
+        {
+            Vector3 p3 = links[i].position;
+            Vector3 tg = (links[i < nbSegments - 1 ? i + 1 : i].position - links[i - 1].position) * (1f / 3f);
+            Vector3 p2 = p3 - tg;
+
+            for (int step = 1; step < nbSteps; step++)
+            {
+                float t = step * one_step;
+                Vector3 q0 = Vector3.Lerp(p0, p1, t);
+                Vector3 q1 = Vector3.Lerp(p1, p2, t);
+                Vector3 q2 = Vector3.Lerp(p2, p3, t);
+                Vector3 r0 = Vector3.Lerp(q0, q1, t);
+                Vector3 r1 = Vector3.Lerp(q1, q2, t);
+                Vector3 b = Vector3.Lerp(r0, r1, t);
+                vertices[nbVertices++] = b;
+            }
+            vertices[nbVertices++] = p3;
+            p0 = p3;
+            p1 = p3 + tg;
+        }
+
+        LineRenderer rend = GetComponent<LineRenderer>();
+        if (rend.numPositions != vertices.Length)
+            rend.numPositions = vertices.Length;
+        rend.SetPositions(vertices);
     }
 }
